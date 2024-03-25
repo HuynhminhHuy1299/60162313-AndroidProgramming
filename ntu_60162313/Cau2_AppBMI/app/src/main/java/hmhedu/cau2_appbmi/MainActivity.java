35ca6004e0fb857c6488d5269aef4acbeb9d2fee
HuynhminhHuy1299 <157135547+HuynhminhHuy1299@users.noreply.github.com>
2024-03-25 13:40:45 +0700

    gắn các điều khiển

--- ntu_60162313/Cau2_AppBMI/app/src/main/java/hmhedu/cau2_appbmi/MainActivity.java
+++ ntu_60162313/Cau2_AppBMI/app/src/main/java/hmhedu/cau2_appbmi/MainActivity.java
@@ -1,6 +1,12 @@
 package hmhedu.cau2_appbmi;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,12 +23,24 @@ public class MainActivity extends AppCompatActivity {
     Button nutTinhBMI, nutReset;
     TextView txtChiSo, txtDanhGia;
     RadioButton radioButtonNam, radioButtonNu;
+    void TimDieuKhien(){
+        editTextHeight = (EditText) findViewById(R.id.edtheight);
+        editTextWeight = (EditText) findViewById(R.id.edtheight);
+        editTextAge = (EditText) findViewById(R.id.edtage);
+        nutTinhBMI = (Button) findViewById(R.id.btntinh);
+        nutReset = (Button) findViewById(R.id.btnreset);
+        txtChiSo = (TextView) findViewById(R.id.txtChiSo);
+        txtDanhGia = (TextView) findViewById(R.id.txtDanhGia);
+        radioButtonNam = (RadioButton) findViewById(R.id.rdbnam);
+        radioButtonNu = (RadioButton) findViewById(R.id.rdbnu);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
+        
     }
 }
