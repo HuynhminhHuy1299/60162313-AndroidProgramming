568e4a59e6eed2ef22087c3dd8c6e176c89f90f2
HuynhminhHuy1299 <157135547+HuynhminhHuy1299@users.noreply.github.com>
2024-03-25 12:33:29 +0700

    Cau2_AppBMI
    khai báo các đối tượng gắn với điều khiển

--- Cau2_AppBMI/app/src/main/java/ntu_60162313/cau2_appbmi/MainActivity.java
+++ Cau2_AppBMI/app/src/main/java/ntu_60162313/cau2_appbmi/MainActivity.java
@@ -1,6 +1,11 @@
 package ntu_60162313.cau2_appbmi;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +13,24 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.text.DecimalFormat;
+
 public class MainActivity extends AppCompatActivity {
+    //Khai báo các đối tượng gắn với điều khiển
+    EditText editTextHeight;
+    EditText editTextWeight;
+    EditText editTextAge;
+    Button nutTinhBMI, nutReset;
+    TextView txtChiSo, txtDanhGia;
+    RadioButton radioButtonNam, radioButtonNu;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
     }
+
+
 }
