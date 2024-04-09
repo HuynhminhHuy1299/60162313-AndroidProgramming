4e30b0df64e6262e7078841a47cfdffa08840187
HuynhminhHuy1299 <157135547+HuynhminhHuy1299@users.noreply.github.com>
2024-04-09 18:33:43 +0700

    RecyclerView phần1
    cập nhật

---
+++ VD_RecyclerView/app/src/main/java/hmh/edu/vd_recyclerview/MainActivity.java
@@ -0,0 +1,45 @@
+package hmh.edu.vd_recyclerview;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    LandScapeAdapter landScapeAdapter;
+    ArrayList<LandScape> recyclerViewDatas;
+    RecyclerView recyclerViewLandscape;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        //3
+        recyclerViewDatas = getDataForRecyclerView();
+        //4
+        recyclerViewLandscape = findViewById(R.id.recyclerLand);
+        //5
+        RecyclerView.LayoutManager layoutLinear = new LinearLayoutManager(this);
+        recyclerViewLandscape.setLayoutManager(layoutLinear);
+        //6
+        landScapeAdapter = new LandScapeAdapter(this, recyclerViewDatas);
+        //7
+        recyclerViewLandscape.setAdapter(landScapeAdapter);
+    }
+    ArrayList<LandScape> getDataForRecyclerView(){
+        ArrayList<LandScape> dsDuLieu = new ArrayList<LandScape>();
+        LandScape landScape1 = new LandScape("eiffel", "Tháp Eiffel");
+        dsDuLieu.add(landScape1);
+        dsDuLieu.add(new LandScape("sand", "Bãi Cát"));
+        dsDuLieu.add(new LandScape("tulipflower", "Hoa Tulip"));
+        dsDuLieu.add(new LandScape("vutru", "Vũ Trụ"));
+        return dsDuLieu;
+    }
+}
